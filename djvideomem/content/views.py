08bec594150168a9832638007bcc0241229ae305
sammy <sammymbevi@gmail.com>
2024-08-29 21:59:08 +0300

    course detail

--- djvideomem/content/views.py
+++ djvideomem/content/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Course
 
 
 class CourseListView(ListView):
     model = Course
     template_name = "content/course_list.html"
+    queryset = Course.objects.all()
+
+
+class CourseDetailView(DetailView):
+    template_name = "content/course_detail.html"
     queryset = Course.objects.all()
